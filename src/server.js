96e45ba0703de6d923dd26abd32eabbf93798210
Nathan Clare <n.clare@youweagency.com>
2025-07-08 15:49:03 +0200

    Second test

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import { WebSocketServer } from 'ws'
 
 const wss = new WebSocketServer({ port: 8080 })
 
-// Broadcast to all clients
 wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
